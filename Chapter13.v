be24cd3318481db99b24e4ca7c46cc23451bd1e9
Jacob Parker <jacob@solidangle.ca>
2021-04-02 09:26:37 -0400

    ch13 solutions

--- Chapter13.v
+++ Chapter13.v
@@ -45,22 +45,36 @@ Theorem ch13p4 :
   mockingbird_exists /\ bluebird_exists /\ warbler_exists
     -> sage_exists.
 Proof.
-  intros [[M HM] [[B HB] [W HW]]].
-Admitted.
+  intros [[M HM] [HBe HWe]].
+  destruct (ch12p3 (conj HBe HWe)) as [L HL].
+  destruct HBe as [B HB].
+  exists (B;M;L). intros x. unfold is_fond_of.
+  rewrite HB. rewrite HM. rewrite <- HL.
+  reflexivity.
+Qed.
 
 Theorem ch13p5 :
   bluebird_exists /\ cardinal_exists /\ warbler_exists
     -> sage_exists.
 Proof.
-  intros [[B HB] [[C HC] [W HW]]].
-Admitted.
+  intros [HBe [[C HC] HWe]].
+  destruct (ch12p3 (conj HBe HWe)) as [L HL].
+  destruct HBe as [B HB].
+  destruct HWe as [W HW].
+  exists (W;(C;(B;B;L);L)). intros x. unfold is_fond_of.
+  rewrite HW. rewrite HC. repeat rewrite HB.
+  rewrite <- HL. reflexivity.
+Qed.
 
 Theorem ch13p6 :
   queer_exists /\ lark_exists /\ warbler_exists
     -> sage_exists.
 Proof.
   intros [[Q HQ] [[L HL] [W HW]]].
-Admitted.
+  exists (W;(Q;L;(Q;L))). intros x. unfold is_fond_of.
+  rewrite HW. repeat rewrite HQ. rewrite <- HL.
+  reflexivity.
+Qed.
 
 (* ch13p7 isn't a new problem *)
 
@@ -68,19 +82,30 @@ Theorem ch13p8_queer_birds_and_mockingbirds :
   queer_exists /\ mockingbird_exists -> sage_exists.
 Proof.
   intros [[Q HQ] [M HM]].
-Admitted.
+  exists (Q;(Q;M);M). intros x.
+  rewrite HQ. unfold is_fond_of.
+  rewrite <- HQ. rewrite <- HM.
+  reflexivity.
+Qed.
 
 Theorem ch13p9_starlings_and_larks :
   starling_exists /\ lark_exists -> sage_exists.
 Proof.
   intros [[S HS] [L HL]].
-Admitted.
+  exists (S;L;L). intros x. unfold is_fond_of.
+  rewrite  HS. rewrite <- HL.
+  reflexivity.
+Qed.
 
 Theorem ch13p10_currys_sage_bird :
   bluebird_exists /\ warbler_exists /\ starling_exists
     -> sage_exists.
 Proof.
-Admitted.
+  intros [HBe [HWe HSe]].
+  (* S;(B;W;B);(B;W;B) -> W;S;(B;W;B) *)
+  pose (conj HBe HWe) as HLe. apply ch12p3 in HLe.
+  apply (ch13p9_starlings_and_larks (conj HSe HLe)).
+Qed.
 
 Definition turing U := forall x y, U;x;y = y;(x;x;y).
 Definition turing_exists := exists U, turing U.
@@ -88,12 +113,21 @@ Definition turing_exists := exists U, turing U.
 Theorem ch13p11_finding_a_turing_bird :
   btm_exists -> turing_exists.
 Proof.
-Admitted.
+  intros H.
+  destruct (ch12p7 H) as [W HW].
+  destruct H as [[B HB] [[T HT] [M HM]]].
+  exists (B;W;(B;(B;T); M)). intros x y.
+  repeat (rewrite HB || rewrite HW).
+  rewrite HT. rewrite HM. reflexivity.
+Qed.
 
 Theorem ch13p12_turing_birds_and_sage_birds :
   turing_exists -> sage_exists.
 Proof.
-Admitted.
+  intros [U HU].
+  exists (U;U). intros x. unfold is_fond_of.
+  rewrite <- HU. reflexivity.
+Qed.
 
 Definition owl O := forall x y, O;x;y = y;(x;y).
 Definition owl_exists := exists O, owl O.
@@ -101,22 +135,38 @@ Definition owl_exists := exists O, owl O.
 Theorem ch13p13_owls :
   queer_exists /\ warbler_exists -> owl_exists.
 Proof.
-Admitted.
+  intros [[Q HQ] [W HW]].
+  exists (Q;Q;W). intros x y.
+  repeat (rewrite HQ || rewrite HW).
+  reflexivity.
+Qed.
 
 Theorem ch13p14 :
   owl_exists /\ lark_exists -> turing_exists.
 Proof.
-Admitted.
+  intros [[O HO] [L HL]].
+  exists (L;O). intros x y.
+  rewrite HL. rewrite HO.
+  reflexivity.
+Qed.
 
 Theorem ch13p15 :
   owl_exists /\ identity_exists -> mockingbird_exists.
 Proof.
-Admitted.
+  intros [[O HO] [I HI]].
+  exists (O;I). intros x.
+  rewrite HO. rewrite HI.
+  reflexivity.
+Qed.
 
 Theorem ch13p16 :
   starling_exists /\ identity_exists -> owl_exists.
 Proof.
-Admitted.
+  intros [[S HS] [I HI]].
+  exists (S;I). intros x y.
+  rewrite HS. rewrite HI.
+  reflexivity.
+Qed.
 
 Theorem ch13p17_a_preliminary_problem :
   forall x y, x <3 y -> x <3 (x;y).
@@ -128,24 +178,35 @@ Qed.
 Theorem ch13p18 O S :
   owl O /\ sage S -> sage (O;S).
 Proof.
-Admitted.
+  intros [HO HS] x. unfold is_fond_of.
+  rewrite HO. repeat rewrite HS.
+  reflexivity.
+Qed.
 
 Theorem ch13p19 O S :
   owl O /\ sage S -> sage (S;O).
 Proof.
-Admitted.
+  intros [HO HS] x. unfold is_fond_of.
+  rewrite <- HO. rewrite HS.
+  reflexivity.
+Qed.
 
 Theorem ch13p20 O A :
   owl O /\ O <3 A -> sage A.
 Proof.
-Admitted.
+  intros [HO HOFA] x. unfold is_fond_of.
+  rewrite <- HO. rewrite HOFA.
+  reflexivity.
+Qed.
 
 Definition choosy A := forall F, A <3 F -> sage F.
 
 Theorem ch13p21 S A :
   sage S /\ choosy A -> sage (S;A).
 Proof.
-Admitted.
+  intros [HS HCA].
+  apply HCA. apply HS.
+Qed.
 
 Definition similar A B := forall x, A;x = B;x.
 Notation "A ~= B" := (similar A B) (at level 50).
@@ -153,7 +214,9 @@ Notation "A ~= B" := (similar A B) (at level 50).
 Theorem ch13p22_similarity O S :
   owl O /\ sage S -> O;S ~= S.
 Proof.
-Admitted.
+  intros [HO HS] x.
+  rewrite HO. apply HS.
+Qed.
 
 Definition extensional_forest :=
   forall A B, A ~= B -> A = B.
@@ -162,4 +225,7 @@ Theorem ch13p23 O :
   owl O /\ extensional_forest
     -> forall S, sage S -> O <3 S.
 Proof.
-Admitted.
+  intros [HO HExt] S HS. unfold is_fond_of.
+  apply HExt. intros x.
+  apply (ch13p22_similarity O S (conj HO HS)).
+Qed.
