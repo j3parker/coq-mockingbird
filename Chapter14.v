f0fae5005268c9d1999a1709acf4c701e4032f8e
Jacob Parker <jacob@solidangle.ca>
2021-04-05 21:22:40 -0400

    ch14 solutions

--- Chapter14.v
+++ Chapter14.v
@@ -1,9 +1,15 @@
 From Mockingbird Require Export Chapter11.
+Require Import Coq.Logic.ClassicalFacts.
 
 Axiom day : Type.
 Axiom sings : bird -> day -> Prop.
 
 Notation "x , d !" := (sings x d) (at level 60).
+
+Definition sing_on_same_days x y :=
+  forall d, x,d! <-> y,d!.
+
+Notation "x ~= y" := (sing_on_same_days x y) (at level 70).
 
 Definition law_1 P := forall x y d,
   y,d! -> P;x;y,d!.
@@ -14,44 +20,56 @@ Definition law_2 P := forall x y d,
 Definition law_3 P := forall x y d,
   x,d! /\ P;x;y,d! -> y,d!.
 
-Definition law_4 P := forall x, exists y, forall d,
-  y,d! <-> P;y;x,d!.
+Definition law_4 P := forall x, exists y,
+  y ~= P;y;x.
 
-Definition special P :=
-  law_1 P /\ law_2 P /\ law_3 P /\ law_4 P.
+Definition special :=
+  exists P, law_1 P /\ law_2 P /\ law_3 P /\ law_4 P.
 
 Definition all_birds_sing_on_all_days :=
   forall x d, x,d!.
 
-Theorem ch14p1 P :
-  special P -> all_birds_sing_on_all_days.
+Theorem ch14p1 :
+  excluded_middle /\ special -> all_birds_sing_on_all_days.
 Proof.
-  unfold special;
-  unfold law_1; unfold law_2; unfold law_3; unfold law_4;
-  intros [H1 [H2 [H3 H4]]] x d.
-  apply (H3 x).
-Admitted.
+  intros [HEM [P [HL1 [HL2 [HL3 HL4]]]]] y d.
+  specialize (HL4 y) as [x HL4].
+  specialize (HL4 d).
+  destruct (HEM (x,d!)) as [Hxd | Hnxd].
+  - apply HL4 in Hxd as HPxyd.
+    apply (HL3 x y d (conj Hxd HPxyd)).
+  - apply (HL2 x y) in Hnxd as HPxyd.
+    apply HL4 in HPxyd. contradiction.
+Qed.
 
-Definition special' P :=
-  law_1 P /\ law_2 P /\ law_3 P.
+Definition special' :=
+  exists P, law_1 P /\ law_2 P /\ law_3 P.
 
-Theorem ch14p2 P :
-  special' P /\ lark_exists -> ~all_birds_sing_on_all_days.
-Proof.
-  intros [[H1 [H2 H3]] HLe] H.
-Admitted.
-
-Theorem ch14p2' P :
-  special' P /\ cardinal_exists -> ~all_birds_sing_on_all_days.
-Proof.
-  intros [[H1 [H2 H3]] HCe] H.
-Admitted.
-
-Theorem ch14p2'' P :
-  special' P /\ cardinal_exists /\ lark_exists
+Theorem ch14p2 :
+  excluded_middle /\ special' /\ cardinal_exists /\ lark_exists
     -> all_birds_sing_on_all_days.
 Proof.
-  intros [[H1 [H2 H3]] [HCe HLe]] x d.
-Admitted.
+  intros [HEM [[P [HL1 [HL2 HL3]]] [[C HC] HLe]]].
+  apply ch14p1. split. apply HEM.
+  exists P. repeat split; try assumption.
+  intros x.
+  destruct (ch9p25 HLe) with (A := C;P;x) as [y].
+  exists y.
+  unfold is_fond_of in H. rewrite HC in H.
+  rewrite H. easy.
+Qed.
 
-(* TODO: p3 *)
+(* TODO: is there a good way to pose this without
+         using the answer? *)
+
+Theorem ch14p3 :
+  excluded_middle /\ special'
+  /\ (exists A, forall x y z, A;x;y;z = x;(z;z);y)
+    -> all_birds_sing_on_all_days.
+Proof.
+  intros [HEM [[P [HL1 [HL2 HL3]]] [A HA]]].
+  apply ch14p1. split. apply HEM.
+  exists P. repeat split; try assumption.
+  intros x. exists (A;P;x;(A;P;x)). intros d.
+  rewrite <- HA. easy.
+Qed.
